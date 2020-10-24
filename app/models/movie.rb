5be87657a4dd56e7dfbe0f7061f1b25a3cc002c6
Jordan Knox <jordanknox100@berkeley.edu>
2020-10-24 20:39:27 +0000

    checking logs

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,5 +5,13 @@ class Movie < ActiveRecord::Base
     return ['G','PG','PG-13','R']
   end
   
+  def self.with_ratings(ratings_list)
+    if ratings_list.length() < 1
+      #return all movies
+    else
+      #return movies with rating in ratings_list
+    end
+  end
+  
   # END JORDANS CODE
 end
